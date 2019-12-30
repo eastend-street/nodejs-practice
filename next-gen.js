433e8363fb09d7f41f347a9041200586bc0af274
eastend-street <ss_1322irf@icloud.com>
2019-12-30 15:33:11 -0700

    added foreach anonymous function

--- next-gen.js
+++ next-gen.js
@@ -9,6 +9,16 @@ const mySum = (...args) => {
 
 const nums = [1, 2, 3, 4];
 
-const myObj = { key1: 15, key2: 30, key3: 45, key4: 60 };
+// console.log(mySum(...nums));
 
-console.log(mySum(...nums));
+const myObj = { key1: 15, key2: 30, key3: 45, key4: 60 };
+const { key1, key2 } = myObj;
+
+// console.log(key1);
+// console.log(key2);
+
+for (let i = 0; i < nums.length; i++) {
+  console.log(nums[i] * 3);
+}
+
+nums.forEach(a => console.log(a * 3));
