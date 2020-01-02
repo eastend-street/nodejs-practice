21d0048cd1a17ee70e0e880533de5217ba630607
eastend-street <ss_1322irf@icloud.com>
2020-01-02 15:32:11 -0700

    made average filter

--- assignments/Jan6/assingment-jan-6.js
+++ assignments/Jan6/assingment-jan-6.js
@@ -11,7 +11,7 @@ const nums = [1, 2, 3, 4];
 console.log(mySum(...nums));
 
 // b) Put this function in a different module and export it.
-// See module folder
+// →See module folder
 
 // c) Create an array of numbers named “myArr”
 const myArr = [1, 2, 3, 4];
@@ -28,10 +28,13 @@ const mySecondArr = myArr.map(num => num * 2);
 
 // f) Filter the contents of your mySecondArr using an anonymous function that console
 // logs the numbers above average number (you’ll need to find the average first).
-const average = console.log(mySecondArr.filter);
+const average = mySum(...mySecondArr) / mySecondArr.length;
+console.log(mySecondArr.filter(num => num > average));
 
 // g) Use setTimeout() function. Pass it a callback function as parameter that console logs
 // “Goodbye” after 3 seconds.
+
+
 // h) Create an “Employee” object that has the following key-value pairs: “name”,
 // “email”, “department” and “startDate”.
 // i) Destructure your object to create a new object, called “Person”, that only has name
