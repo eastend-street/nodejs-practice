7d0dc34eab2954edd6a2b24cc30c5aff1f71386d
eastend-street <ss_1322irf@icloud.com>
2020-01-01 18:54:23 -0700

    changed directory structure

---
+++ assignments/Jan6/assingment-jan-6.js
@@ -0,0 +1,13 @@
+// a) Create an arrow function named “mySum”
+const mySum = (...args) => {
+  let base = 0;
+
+  for (i in args) {
+    base = base + args[i];
+  }
+  return base;
+};
+
+const nums = [1, 2, 3, 4];
+
+console.log(mySum(...nums));
