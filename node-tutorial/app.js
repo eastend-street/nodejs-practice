c3012f3c612e5af2623874e2cebaaddc987070f5
eastend-street <ss_1322irf@icloud.com>
2020-01-07 13:50:50 -0800

    added package.json

--- node-tutorial/app.js
+++ node-tutorial/app.js
@@ -1,8 +1,9 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
-
+  console.log(__dirname);
+  console.log(__filename);
+  res.setHeader("Content-type", "text/html");
   res.write("<h1>hello node</h1>");
   res.end();
 });
