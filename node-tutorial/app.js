e27faba34bc39f8f840b76fca68a27ad32478c23
eastend-street <ss_1322irf@icloud.com>
2020-01-07 13:36:39 -0800

    added response

--- node-tutorial/app.js
+++ node-tutorial/app.js
@@ -2,6 +2,9 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log(req);
+
+  res.write("<h1>hello node</h1>");
+  res.end();
 });
 
 server.listen(3000);
