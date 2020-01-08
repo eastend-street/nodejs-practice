4ea294ef1f9ab3b68114b57aea0182670f8e9c10
eastend-street <ss_1322irf@icloud.com>
2020-01-08 13:57:52 -0800

    removed route function from app.js

--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -7,30 +7,4 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", (req, res, next) => {
-  res.setHeader("Content-type", "text/html");
-  res.write(
-    '<form action="/" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form>'
-  );
-  res.end();
-});
-
-app.use("/", (req, res, next) => {
-  console.log("2nd middleware");
-});
-
-app.post("/", (req, res, next) => {
-  console.log(req.body);
-
-  fs.writeFile("message.txt", req.body.message, () => {
-    res.status(302);
-    res.redirect("/success");
-    res.end();
-  });
-});
-
-app.get("/success", (req, res, next) => {
-  res.write("<h1>success</h1>");
-});
-
 app.listen(3000);
