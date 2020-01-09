55e354e6fea15e3245ece69a76d4267f530cbbca
eastend-street <ss_1322irf@icloud.com>
2020-01-09 13:30:18 -0800

    added ejs

---
+++ ejs-tutorial/app.js
@@ -0,0 +1,14 @@
+// imports
+const express = require("express");
+const parser = require("body-parser");
+const router = require("./routes/main-router");
+
+// Set up
+const app = express();
+app.set("view engine", "ejs")
+
+// Middle wares
+app.use(parser.urlencoded({ extended: false }));
+app.use(router);
+
+app.listen(3000);
