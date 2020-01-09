93e96a34e1c8f1f677fc3515d87a545f229634a8
eastend-street <ss_1322irf@icloud.com>
2020-01-09 13:41:50 -0800

    made dynamic html

--- ejs-tutorial/routes/main-router.js
+++ ejs-tutorial/routes/main-router.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
+const data = [];
+
 router.get("/", (req, res, next) => {
-  console.log("welcome");
+  res.render("index", { notes: data });
+  console.log(data);
+});
+
+router.post("/note", (req, res, next) => {
+  data.push(req.body);
+  res.status(302);
+  res.redirect("/");
 });
 
 module.exports = router;
