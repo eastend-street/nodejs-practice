2225f0f7821a15946d1c8d1e81c945fca9c6399b
eastend-street <ss_1322irf@icloud.com>
2020-01-17 13:59:45 -0800

    added express_crud

--- express_crud/app.js
+++ express_crud/app.js
@@ -1,17 +1,20 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const router = require('./routes/route');
+const express = require("express");
+const bodyParser = require("body-parser");
+require('dotenv').config();
+const database = require("./util/database");
+const router = require("./routes/route");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(router);
 
+app.use(express.static("./public"));
 
-app.use(express.static('./public'));
-
-app.listen(3000);
+// app.listen(3000);
+database.mongoConnect(() => {
+  app.listen(3000);
+});
--- express_crud/models/note.js
+++ express_crud/models/note.js
@@ -1,62 +1,60 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
+const database = require("../util/database");
 
-const p = path.join(__dirname, '../data', 'notes.json');
+const p = path.join(__dirname, "../data", "notes.json");
 
 class Note {
-    constructor(author, title, content) {
-        this.author = author;
-        this.title = title;
-        this.content = content;
-    }
+  constructor(author, title, content) {
+    this.author = author;
+    this.title = title;
+    this.content = content;
+  }
 
-    saveNote() {
-        this.id = Math.random().toString();
+  saveNote() {
+    let db = database.getDB();
+    db.collection("notes")
+      .insertOne(this)
+      .then(result => {
+        console.log(result);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 
-        fs.readFile(p, (err, data) => {
-            const noteArr = JSON.parse(data);
-            console.log(noteArr);
-            noteArr.push(this);
+  static getAll(callback) {
+    fs.readFile(p, (err, data) => {
+      if (err) {
+        callback([]);
+      }
+      callback(JSON.parse(data));
+    });
+  }
 
-            fs.writeFile(p, JSON.stringify(noteArr), (err) => {
-                console.log(err);
-            })
+  static deleteNote(id) {
+    this.getAll(notes => {
+      const updatedNotes = notes.filter(nt => nt.id != id);
+      console.log(updatedNotes);
 
-        });
-    }
+      fs.writeFile(p, JSON.stringify(updatedNotes), (err, data) => {
+        console.log("hey", data);
+      });
+    });
+  }
 
-    static getAll(callback) {
-        fs.readFile(p, (err, data) => {
-            if (err) {
-                callback([])
-            }
-            callback(JSON.parse(data));
-        });
-    }
+  static updateNote(id, title, content) {
+    this.getAll(notes => {
+      const noteToChange = notes.find(nt => nt.id === id);
 
-    static deleteNote(id) {
-        this.getAll((notes) => {
-            const updatedNotes = notes.filter((nt) => nt.id != id)
-            console.log(updatedNotes);
-              
-            fs.writeFile(p, JSON.stringify(updatedNotes), (err, data) => {
-                console.log('hey', data);
-            });
-        });
-    }
+      noteToChange.title = title;
+      noteToChange.content = content;
 
-    static updateNote(id, title, content) {
-        this.getAll((notes) => {
-            const noteToChange = notes.find(nt => nt.id === id);
-            
-            noteToChange.title = title;
-            noteToChange.content = content;
-            
-            fs.writeFile(p, JSON.stringify(notes), (err, data) => {
-                console.log('hey', data);
-            });
-        });
-    }
+      fs.writeFile(p, JSON.stringify(notes), (err, data) => {
+        console.log("hey", data);
+      });
+    });
+  }
 }
 
 module.exports = Note;
